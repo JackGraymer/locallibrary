c3e9f5c3e5b1d9feaa1543c2707a05600bfc66bc
Jack Graymer <jackgraymer@gmail.com>
2023-05-12 22:48:30 +0200

    Adds controller to book list route, populates with db info

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -34,7 +34,12 @@ exports.index = asyncHandler(async (req, res, next) => {
 
 // Display list of all books.
 exports.book_list = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Book list");
+  const allbooks = await Book.find({}, "title author")
+    .sort({ title: 1 })
+    .populate("author")
+    .exec();
+
+  res.render("book_list", { title: "Book List", book_list: allbooks });
 });
 
 // Display detail page for a specific book.
